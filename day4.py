1639d0b2cff3bce314156ca09ce3c375b36ffb02
noob2420 <74348368+noob2420@users.noreply.github.com>
2021-06-15 21:21:47 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,26 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = b = c = 50
+
+#Divide a by 10
+divi = a/10
+print(int(divi))
+
+#Multiply b by 50
+mul = b * 50
+print(mul)
+
+#Add c value by 60
+add_1 = c + 60
+print(add_1)
+
+#Create a String variables (a,b) of 5 characters and replace the 3rd character with G
+var_1 = "abcde"
+x = var_1.replace(var_1[2],"G")
+print(x)
+
+#Create two values (a,b) of int,float data type & convert the vise versa
+a,b = 30.5,50.5
+a1 = (float(a))
+b1 = (int(b))
+print(a1,b1)
