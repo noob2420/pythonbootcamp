7fe81b2f21c6fea8c5653f95f4403a92a12d1366
noob2420 <74348368+noob2420@users.noreply.github.com>
2021-06-20 12:46:49 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,11 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("Enter integer 1:"))
+b=int(input("ENTER INTEGER 2 :"))
+print("Addition of integers : ",a+b)
+print("Subtraction of integers : ",a-b)
+print("Multiplication of integers : ",a*b)
+print("Division of integers : ",a/b)                                                                                                                            #question2                                                                                     def covid():
+    a=str(input("Enter Patient Name : "))
+    b=float(input("Enter body temperature : "))
+    b=98
